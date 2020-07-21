329ef5e0700a72c27a8f899eed008b324ac13ae8
Shirlen Detablan <slen.detablan@gmail.com>
2020-07-21 16:29:55 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -29,7 +29,7 @@ def can_be_created_with_a_hash_of_attributes
   
 end
 
-def can_be_created_in_a_block(args = )
+def can_be_created_in_a_block(args = {  })
   
   # If no arguments are passed, use default values:
   # title == "Home Alone"
