ebaf3f1fd1a56886d18e2fe1067a8915936505d9
Shirlen Detablan <slen.detablan@gmail.com>
2020-07-21 16:15:30 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -21,7 +21,8 @@ def can_be_created_with_a_hash_of_attributes
       lead: "Paul Newman",
       in_theaters: false
   }
-  movie = __
+  movie = Movie.new 
+  movie = movie.
 end
 
 def can_be_created_in_a_block(args = __)
