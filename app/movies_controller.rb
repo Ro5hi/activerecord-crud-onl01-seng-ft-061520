88742dc1642c99506585da288c52717393e60ed4
Shirlen Detablan <slen.detablan@gmail.com>
2020-07-21 16:15:46 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -22,7 +22,9 @@ def can_be_created_with_a_hash_of_attributes
       in_theaters: false
   }
   movie = Movie.new 
-  movie = movie.
+  movie.save 
+  movie 
+  
 end
 
 def can_be_created_in_a_block(args = __)
